c8a96582ef06d1020e16fd13ab9129307cb552a5
Sébastien Delafond <sdelafond@gmail.com>
2021-01-21 07:50:34 +0100

    build: can't use pipefail in combination with "foo | something-that-exits-early"

--- common.sh
+++ common.sh
@@ -1,5 +1,5 @@
 get_openwrt_version() {
-  git describe --tags --abbrev=0 --match 'v[0-9][0-9].[0-9][0-9]*' 2> /dev/null | sed -e 's/^v//' || git log --decorate --pretty=oneline -n 10 | perl -lne 'if (m/^[a-f0-9]+ \(.+?\/(.+)\)/) {print $1 ; exit}'
+  git describe --tags --abbrev=0 --match 'v[0-9][0-9].[0-9][0-9]*' 2> /dev/null | sed -e 's/^v//' || git log -n 1 --pretty=%D  | perl -pe 's/.*, .+?\/(.+)/$1/'
 }
 
 get_mfw_version() {
